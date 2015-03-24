d4bdacbef03043262bfbd7e6522314010318e094
Mikolaj Muller <mikolaj.muller@ilim.poznan.pl>
2015-03-24 12:35:37 +0100

    added ng-table

--- web/app.js
+++ web/app.js
@@ -1,1 +1,1 @@
-var app=angular.module('mm',[]);
+var app=angular.module('mm',['ngTable']);
--- web/core.js
+++ web/core.js
@@ -1,11 +1,12 @@
-app.controller('logs_ctrl',function($scope,$http,$interval){
-  load_pictures();
-  $interval(function(){
-    load_pictures();
-  },30000);
-  function load_pictures(){
-  $http.get('http://localhost:3000/logs').success(function(data){    
-    $scope.logs=data;
-  });
-  };
+app.controller('logs_ctrl',function($scope,$http,$interval, ngTableParams){
+
+	load_data();
+	$interval(function(){
+		load_pictures();
+	},30000);
+	function load_data(){
+		$http.get('http://localhost:3000/logs').success(function(data){    
+			$scope.logs=data;
+		});
+	};
 });
